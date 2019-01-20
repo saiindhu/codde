023eab9ba097d453c1233f911ff8fbd1159c7ae8
saiindhu <46838993+saiindhu@users.noreply.github.com>
2019-01-20 16:09:18 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+ int i,n,sum=0,k,a[10];
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+ {
+    scanf("%d",a[i]);
+ }
+ sum=0;
+ for(i=0;i<=k;i++)
+ {
+     sum=sum+i;
+ }
+ printf("%d",sum);
+ getch();
+}
