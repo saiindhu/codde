1254d5452a7c492a6d7003b3c00484a312122572
saiindhu <46838993+saiindhu@users.noreply.github.com>
2019-01-28 17:23:01 +0530

    numfind.c

---
+++ numfind.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s[10],i;
+scanf("%s",&s);
+if(((s[i]>=65) && (s[i]<=91)) || ((s[i]>=97) && (s[i]<=122)))
+{
+printf("No");
+}
+else
+{
+    printf("Yes");
+}
+getch();
+}
