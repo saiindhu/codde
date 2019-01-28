1c718052b168c917588ad3edc346e588f77bdf1d
saiindhu <46838993+saiindhu@users.noreply.github.com>
2019-01-28 13:34:37 +0530

    rangenum.c

---
+++ rangenum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>1 && a<10)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    getch();
+}
